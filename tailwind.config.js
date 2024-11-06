c1341ad2f6345c986e22e193353333fb96eb99aa
Avi074 <gautamavinash.ag@gmail.com>
2024-11-06 08:01:47 +0530

    vite react & material tailwind configured

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+import withMT from "@material-tailwind/react/utils/withMT"
+
+/** @type {import('tailwindcss').Config} */
+const tailwindConfig = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: "var(--color-primary-100)",
+          200: "var(--color-primary-200)",
+          300: "var(--color-primary-300)",
+          400: "var(--color-primary-400)",
+          500: "var(--color-primary-500)",
+          600: "var(--color-primary-600)",
+          700: "var(--color-primary-700)",
+        },
+        neutral: {
+          100: "#f8f9fa",
+          200: "#e9ecef",
+          300: "#dee2e6",
+          400: "#ced4da",
+          500: "#adb5bd",
+          600: "#6c757d",
+          700: "#495057",
+          800: "#343a40",
+          900: "#212529",
+        },
+      },
+      screens: {
+        xs: "360px",
+        sm: "540px",
+        md: "720px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default withMT(tailwindConfig)
