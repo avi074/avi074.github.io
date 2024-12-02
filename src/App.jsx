78524fbdcda303a27b4e2925067bd6f5b95a35b5
avi074 <gautamavinash.ag@gmail.com>
2024-12-02 22:34:29 +0530

    Initial Setup with vite-react & shadecn ui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <>
+    <div className="p-4">
+      <h1 className="text-3xl font-bold text-red-600">Welcome to ShadeCN with Vite + React!</h1>
+      <Button>Click Me</Button>
+    </div> 
+    </>
+  )
+}
+
+export default App
